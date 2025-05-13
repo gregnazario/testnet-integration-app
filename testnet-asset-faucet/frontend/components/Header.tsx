342d19e848389691e858b0db484444f6947c0258
Greg Nazario <greg@gnazar.io>
2025-05-13 10:47:33 -0400

    add source code link

--- testnet-asset-faucet/frontend/components/Header.tsx
+++ testnet-asset-faucet/frontend/components/Header.tsx
@@ -4,7 +4,16 @@ export function Header() {
   return (
     <div className="flex items-center justify-between px-4 py-2 max-w-screen-xl mx-auto w-full flex-wrap">
       <h1 className="display">Testnet Test Asset Faucet</h1>
-
+      <div className="flex gap-2 items-center flex-wrap">
+        <a
+          href={"https://github.com/gregnazario/testnet-integration-app/tree/main/testnet-asset-faucet"}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-sm text-gray-500 hover:text-gray-700"
+        >
+          Source Code
+        </a>
+      </div>
       <div className="flex gap-2 items-center flex-wrap">
         <WalletSelector />
       </div>
